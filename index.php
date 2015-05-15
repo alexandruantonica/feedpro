6a40e6add981ac48030b0ad42a3f1cc1fae8cecf
Antonica <alexandru.antonica@gmail.com>
2015-05-15 16:22:57 +0300

    Style

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antonica
+ * Date: 15.05.2015
+ * Time: 16:18
+ */
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FeedPro</title>
+</head>
+<body>
+    
+</body>
+</html>
